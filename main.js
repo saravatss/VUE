e26ed4a5f8647cee6f8b0aa40e1b9ccdedd42fce
saravatss <rebback89@gmail.com>
2024-01-18 17:08:03 +0300

    button

--- main.js
+++ main.js
@@ -1,80 +1,28 @@
-const { 
-    ref, 
-    computed, 
-    createApp,
-} = Vue;
+const { createApp, ref } = Vue;
 
-const Root = {
+// tempate -- тело
+// setup -- настройки js
+
+const app = createApp({
     template: `
-        <div>
-            <div>
-                <h3>Ваша фамилия:</h3> 
-                <input type="text" v-model="formModel.lastName">
+        <button @click="onClick">
+            +
+        </button>
 
-                <h3>Ваше имя:</h3> 
-                <input type="text" v-model="formModel.name">
-
-                <h3>Ваше отчество:</h3> 
-                <input type="text" v-model="formModel.middleName">
-                
-                <br>
-
-                <h3>Сколько лет:</h3> 
-                <input type="number" v-model="formModel.age">
-
-                <br>
-
-                <input type="radio" v-model="formModel.gender" value="мужчина">М
-                <input type="radio" v-model="formModel.gender" value="женщина">Ж
-
-                <br>
-
-                <h3>Дата рождения:</h3> 
-                <input type="date" v-model="formModel.date">
-        
-                <br>
-                
-            </div>
-
-            <br>
-
-            <button @click="onClick">
-                Отправить форму
-            </button>
-
-            <br>
-            <br>
-
-            Ваше ФИО: {{ formModel.lastName }} {{ formModel.name }} {{ formModel.middleName }}
-            <br>
-            Вам {{ formModel.age }} лет
-            <br>
-            Вы {{ formModel.gender }}
-            <br>
-            Ваше др: {{ formModel.date }}
-        </div>
+        <div v-show="isVisible">hfjdk</div>
     `,
     setup () {
-        const formModel = ref({
-            lastName: '',
-            name: '',
-            middleName: '',
-            age: '',
-            gender: '',
-            date: ''
-        });
+        const isVisible = ref(false);
 
         function onClick () {
-            console.log(formModel.value);
+            isVisible.value = !isVisible.value;
         }
 
         return {
-            formModel,
+            isVisible,
             onClick
         }
     }
-};
-
-const app = createApp(Root);
+});
 
 app.mount('#app');
